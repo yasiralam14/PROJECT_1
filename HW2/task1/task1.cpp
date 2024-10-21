a768a05bc2a091379a99be3b06eb1301755031fa
Syed <yasir.alam14@gmail.com>
2024-10-21 17:10:53 -0400

    The new updates relate to progress made in task1 as well as task 2

--- HW2/task1/task1.cpp
+++ HW2/task1/task1.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+#include <random>
+#include <cstdlib> 
+
 
 int main() {
-    std::cout << "hello formatting";
+    std::random_device random_device;
+    std::mt19937 gen(random_device());
+    std::uniform_int_distribution<> dist(1, 99);
 
-    return 0;
+    int guess = dist(gen);
+    std::cout << "Please enter an integer " << std::endl;
+    int inputnumber;  
+    std::cin >> inputnumber;
+
+    do {
+        
+        
+        if (std::cin.fail()){
+            std::cerr << "“Error encountered, exiting..." << std::endl;
+            std::cin.clear();
+            std::cin.ignore();
+            return EXIT_FAILURE;
+        }
+        if (inputnumber > 99 || inputnumber < 0 ){
+            std::cerr << "WARNING] : Number must be between 0 and 99" << std::endl;
+            std::cin >> inputnumber;
+        }
+        else if (inputnumber == guess) {
+            std::cout << "You got it right" << std::endl;
+            break;
+
+        } else if (inputnumber > guess) {
+            std::cout << "Your number is bigger than the guess" << std::endl;
+            std::cin >> inputnumber;
+
+        } else {
+            std::cout << "Your number is smaller than the guess" << std::endl;
+            std::cin >> inputnumber;
+        }
+
+    } while (true);
+
+    EXIT_SUCCESS;
 }
+
---
+++ HW2/task2/task2.cpp
@@ -0,0 +1,65 @@
+#include <cstdlib>
+#include <iostream>
+#include <random>
+#include <sstream>
+#include <string>
+#include <vector>
+
+// executable should be named task_1
+// have a look at what random device is in c++
+// have a look at what cin.fail() is in c++
+// right the code that takes in a number from you it already has a guess with it
+// and compare your number with its guess and then gives you three outputs the
+// standard ones you can guess. the main thing is the cin should flow until the
+// guess has been made
+
+int main() {
+    std::string input;
+    std::getline(std::cin, input);
+    std::cout << input << std::endl;
+
+    std::istringstream iss(input);
+    std::vector<std::string> args;
+    std::string arg;
+
+    while (iss >> arg) {
+        args.push_back(arg);
+    }
+
+    if (args.size() != 2) {
+    std:
+        std::cerr << "You did not give the right amount of arguments";
+        EXIT_FAILURE;
+    }
+    std::string ext1;
+    std::string ext2;
+    int file_name1;
+    int file_name2;
+
+    std::size_t dot_pos_1 = args[0].find_last_of('.');
+    if (dot_pos_1 != std::string::npos) {
+        ext1 = args[0].substr(dot_pos_1 + 1);
+        file_name1 = std::stoi(args[0].substr(0,dot_pos_1));
+    } else {
+        std::cout << "Wrong Input" << std::endl;
+        EXIT_FAILURE;
+    }
+    std::size_t dot_pos_2 = args[1].find_last_of('.');
+    if (dot_pos_2 != std::string::npos) {
+        ext2 = args[1].substr(dot_pos_2 + 1);
+        file_name2 = std::stoi(args[1].substr(0, dot_pos_2));
+    } else {
+        std::cout << "Wrong Input" << std::endl;
+        EXIT_FAILURE;
+    }
+
+    if (ext1 == ".txt" && ext2 == ".txt") {
+    } else if (ext1 == ".png" && ext2 == ".png") {
+    } else if (ext1 == ".txt" && ext2 == ".png") {
+    } else {
+        std::cout << "Wrong input format" << std::endl;
+        EXIT_FAILURE;
+    }
+
+    EXIT_SUCCESS;
+}
